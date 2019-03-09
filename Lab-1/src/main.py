3c681f3e09ce1db25ced20e7c8539d46a7238143
LokoElena <loko.elena@mail.ru>
2019-03-09 12:08:19 +0700

    Added the implementation of graphs

--- Lab-1/src/main.py
+++ Lab-1/src/main.py
@@ -4,9 +4,8 @@ import sys
 import math
 import numpy as np
 import random as rnd
-from math import exp
 from scipy import integrate
-from math import cos, pi
+import matplotlib.pyplot as plt
 
 a = -0.5379
 n = 100
@@ -31,36 +30,48 @@ def func(x):
     return y
 
 def gen_ran():
-    rand_dots = np.zeros([1, 20])
+    rand_dots = np.zeros([1, 100])
     for i in range (len(rand_dots[0])):
         rnd.seed(i, version=2)
         rand_dots[0][i] = rnd.uniform(-6, 0)
-    fin_arr = np.zeros([2, 20])
-
-    print(rand_dots)
-    print("----------------------")
-    print(fin_arr)
-
+    fin_arr = np.zeros([2, 100])
     pos = 0
-    f = 2
 
     for i in range (len(rand_dots[0])):
-        if not (func(rand_dots[0][i])):
+        if (func(rand_dots[0][i])):
             fin_arr[0, pos] = rand_dots[0, i]
-            fin_arr[1, pos] = rand_dots[0, i] * f
+            fin_arr[1, pos] = func(rand_dots[0, i])
             pos += 1
-
-    print("----------------------")
-    print(fin_arr)
-
+    return fin_arr
 
 def main(argv = None):
     rnd.seed(10, version=2)
     mathexp = MathExp_Disper(v, -4, 5*a, n)
     disper = MathExp_Disper(w, -4, 5*a, n)
+
     print("MathExp", mathexp)
     print("Dispersion", disper - mathexp**2)
     print("Square deviation", pow(disper - mathexp**2, .5))
-    gen_ran()
+
+    rnd_arr = np.zeros([2, 100])
+    rnd_arr_x = np.zeros([1, 100])
+    rnd_arr_y = np.zeros([1, 100])
+    rnd_arr = gen_ran()
+
+    for i in range (len(rnd_arr[0])):
+        rnd_arr_x[0][i] = rnd_arr[0][i]
+        rnd_arr_y[0][i] = rnd_arr[1][i]
+
+    x2 = np.arange(-4, 4*np.pi+0.1, 0.1)
+    y2 = np.sqrt(x2+4)
+
+    fig = plt.figure()
+    plt.plot(x2, y2, color='pink', linewidth=4.0)
+    plt.plot(rnd_arr_x[0], rnd_arr_y[0], color='g', linewidth=4.0)
+
+
+    plt.grid(True)
+    plt.show()
+    print("FIN ", rnd_arr)
 
 main()
