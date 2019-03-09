aa919bd47ef314a7acdad6a07b5adfc8d749d1e1
Doshinsky <akter1997@gmail.com>
2019-03-09 12:39:34 +0700

    Finil version of the first lab

--- Lab-1/src/main.py
+++ Lab-1/src/main.py
@@ -30,18 +30,18 @@ def func(x):
     return y
 
 def gen_ran():
-    rand_dots = np.zeros([1, 100])
+    rand_dots = np.zeros([1, 1000])
     for i in range (len(rand_dots[0])):
         rnd.seed(i, version=2)
-        rand_dots[0][i] = rnd.uniform(-6, 0)
-    fin_arr = np.zeros([2, 100])
+        rand_dots[0][i] = rnd.uniform(-10, 10)
+    fin_arr = np.zeros([3, 1000])
     pos = 0
 
     for i in range (len(rand_dots[0])):
-        if (func(rand_dots[0][i])):
-            fin_arr[0, pos] = rand_dots[0, i]
-            fin_arr[1, pos] = func(rand_dots[0, i])
-            pos += 1
+        fin_arr[0, pos] = rand_dots[0, i]
+        fin_arr[1, pos] = func(rand_dots[0, i])
+        pos += 1
+    fin_arr[2, 0] = pos
     return fin_arr
 
 def main(argv = None):
@@ -53,25 +53,22 @@ def main(argv = None):
     print("Dispersion", disper - mathexp**2)
     print("Square deviation", pow(disper - mathexp**2, .5))
 
-    rnd_arr = np.zeros([2, 100])
-    rnd_arr_x = np.zeros([1, 100])
-    rnd_arr_y = np.zeros([1, 100])
+    rnd_arr = np.zeros([2, 1000])
     rnd_arr = gen_ran()
-
-    for i in range (len(rnd_arr[0])):
+    cnt = rnd_arr[2][0]
+    rnd_arr_x = np.zeros([1, int(cnt)])
+    rnd_arr_y = np.zeros([1, int(cnt)])
+    for i in range(int(cnt)):
         rnd_arr_x[0][i] = rnd_arr[0][i]
         rnd_arr_y[0][i] = rnd_arr[1][i]
 
-    x2 = np.arange(-4, 4*np.pi+0.1, 0.1)
+    x2 = np.arange(-4, 4*np.pi + 0.1, 0.1)
     y2 = np.sqrt(x2+4)
 
     fig = plt.figure()
-    plt.plot(x2, y2, color='pink', linewidth=4.0)
-    plt.plot(rnd_arr_x[0], rnd_arr_y[0], color='g', linewidth=4.0)
-
-
+    plt.plot(x2, y2, color='red', linewidth=4.0)
+    plt.plot(rnd_arr_x[0], rnd_arr_y[0], 'bo', color='blue')
     plt.grid(True)
     plt.show()
-    print("FIN ", rnd_arr)
 
 main()
