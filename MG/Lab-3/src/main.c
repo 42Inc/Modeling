694c7f7e1839dd972b914496a3c6f729747754a3
Doshchinsky <akter1997@gmail.com>
2020-04-17 20:44:29 +0700

    HMM

--- MG/Lab-3/src/main.c
+++ MG/Lab-3/src/main.c
@@ -10,29 +10,24 @@ int generator(int n, double** matrix) {
    * Если размерность матрицы меньше или 0 - ошибка.
    * Если указатель - NULL, тоже.
    * */
-  if (n <= 0 || !matrix) {
+  if (n <= 0 || !matrix)
     return 1;
-  }
-  for (i = 0; i < n; ++i) {
-    if (!matrix[i]) {
+
+  for (i = 0; i < n; ++i)
+    if (!matrix[i])
       return 2;
-    }
-  }
 
   /* Процесс генерации. Заполняем матрицу значениями 1/n */
-  for (i = 0; i < n; ++i) {
-    for (j = 0; j < n; ++j) {
+  for (i = 0; i < n; ++i)
+    for (j = 0; j < n; ++j)
       matrix[i][j] = 1.0 / n;
-    }
-  }
 
   /* Печать матрицы в 2 */
   for (i = 0; i < n; ++i) {
-    for (j = 0; j < n; ++j) {
+    for (j = 0; j < n; ++j)
       fprintf(stderr, "%.02lf ", matrix[i][j]);
-    }
-      fprintf(stderr, "\n");
-  }
+    fprintf(stderr, "\n");
+	}
   return 0;
 }
 
@@ -51,16 +46,14 @@ int main(int argc, char** argv) {
 
   matrix = malloc(n * sizeof(double));
   /* Не выделилась память */
-  if (!matrix) {
-    return 255;
-  }
+  if (!matrix) return 255;
+
   for (i = 0; i < n; ++i) {
     matrix[i] = malloc(n * sizeof(double));
     /* Не выделилась память */
     if (!matrix[i]) {
-      for (j = 0; j < i; ++j) {
+      for (j = 0; j < i; ++j)
         free(matrix[j]);
-      }
       free(matrix);
       return 254;
     }
@@ -69,24 +62,21 @@ int main(int argc, char** argv) {
   /* Если задан вызов из генератора */
   if (argv[2] && !strcmp(argv[2], "gen")) {
     m = gen;
-  } else if (argv[2] && !strcmp(argv[2], "file")) {
+	} else if (argv[2] && !strcmp(argv[2], "file")) {
     /* Если вызов из файла */
-    if (argv[3]) {
+    if (argv[3])
       filename = argv[3];
-    }
     m = file;
   } else {
     m = gen;
   }
   /* Вызов генератора матрицы. Вернет !0, если будет ошибка */
   if (m == gen) {
-    if (generator(n, matrix)) {
-      return 253;
-    }
+    if (generator(n, matrix)) return 253;
   } else {
     /* Чтение из файла*/
   }
 
   /* TODO: Имитация переходов*/
-  return 0;
+  return EXIT_SUCCESS;
 }
