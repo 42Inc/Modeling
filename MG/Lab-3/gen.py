a5f690ab4bcf6cff2c0ca650c61463cebd425132
Doshchinsky <akter1997@gmail.com>
2020-04-18 20:30:14 +0700

    Watch gen.py

--- MG/Lab-3/gen.py
+++ MG/Lab-3/gen.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env python3
 
-filePath = "matrix.dat"	
-N = 10						
+filePath = "data/matrix.dat"
+firstString = [0.4, 0.5, 0.1, 0, 0]
+matrixSize = len(firstString)
 
 def genDist():
 	probCon = 0
-	pathArray = [[-255 for cnt in range(citiesNum)]\
-				for cnt in range(citiesNum)]
-
+	pathArray = [[-255 for cnt in range(matrixSize)]\
+				for cnt in range(matrixSize)]
+	pathArray[0] = firstString
 	# Generating distances for pathArray
-	for i in range(1, N):
-		for j in range(N):
-
+	for i in range(1, matrixSize):
+		for j in range(matrixSize):
+			if (i == 0):
+				pathArray[i][j] = pathArray[matrixSize-1][j-1]
+			else:
+				pathArray[i][j] = pathArray[i-1][j-1]
+	print(pathArray)
 	saveData(pathArray)
 
 def saveData(pathData):
 	with open(filePath, 'w', encoding='utf-8') as graphFile:
 		for row in pathData:
 			for path in row:
-				graphFile.write("%d\t" % path)
+				graphFile.write("%.1f\t" % path)
 			graphFile.write("\n")
 
 
