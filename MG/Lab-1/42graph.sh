6689ce8008c225bf3829720574fcdc2c28229118
Alexey R <alexey_r.98@ngs.ru>
2020-05-10 02:42:34 +0700

    Final?

--- MG/Lab-1/42graph.sh
+++ MG/Lab-1/42graph.sh
@@ -7,7 +7,10 @@ set border linewidth 1
 set grid
 set mytics
 set mxtics 
-set yrange [0:5] 
+set yrange [0:*] 
+set xrange [0:*] 
+# set xrange [0.4:0.8] 
+# set yrange [0:5] 
 # set format y "%d"
 set xlabel "x" font "Arial, 16"
 # set format x "%d"
@@ -16,6 +19,16 @@ set ylabel "y" font "Arial, 16"
 # set ytics font "Arial, 12"
 # set rmargin 4
 # set tmargin 2 
+# set xtics 0.05
+set boxwidth 0.05 absolute
+set style fill solid 1.0 noborder
+
+bin_width = 0.001;
+
+bin_number(x) = floor(x/bin_width)
+
+rounded(x) = bin_width * ( bin_number(x)  )
+
 plot  \
-     "log_origin.dat" using 1:2 notitle linecolor rgb 'red',\
-     "log_a.dat" using 1:2 notitle linecolor rgb 'green'
+     "log_origin.dat" using 1:2 notitle linecolor rgb 'red' ,\
+     "log.dat" using (rounded($1)):(1) notitle smooth frequency linecolor rgb 'blue'
--- MG/Lab-1/main.c
+++ MG/Lab-1/main.c
@@ -1,101 +1,86 @@
+#define _USE_MATH_DEFINES
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-#define EPS 1e+3
-#define CNT 1e+5
-#define MIN 0
-#define MAX 1
+// #define EPS 1e+3
+#define CNT 100000
 #define ln log
 
 const double e = 2.71828182890459027;
-const double a = 1.0;
 /*
  * Task
- * a      | [0; a_x]
+ * a       | [0; a_x]
  * 4e^(-4x)| (a_x; +inf)
  * a_x можно выразить: Известно, что 4e^(-4a_x) = a
  * Отсюда:
  * a/4 = e^(-4a_x) => ln(a/4) = ln(e^(-4a_x)) => ln(a/4) = -4a_x =>
  * a_x = -ln(a/4)/4
  *
- * Теперь интереснее. Берем интеграл кусочной функции.
- * (Интеграл как I(a,b))
- * I(0, a_x) = a * x         | [0; a_x]
- * I(a_x, +inf{x}) = e^(-4a_x) - e^(-4x)  + a * a_x  | (a_x; +inf)
- *
- * I(a_x, +inf{x}) = a/4 - e^(-4x)  + a * a_x
- * a * (a_x + 0.25) - e^(-4x)
+ * Теперь интереснее. Берем интеграл кусочной функции. И находим ФР.
+ * (Интеграл как I(a,b)). А ещё смешнее то,
+ * что на [0; a_x] равномерное распределение.
+ * I(0, x) = a * x                        | [0; a_x]
+ * I(a_x, +inf{x}) = e^(-4*a_x) - e^(-4x) | (a_x; +inf)
  *
  * И переходим к магии. Имеем СВ k, равномерно распределенную в [0:1].
  * И приравниваем к нашей функции распределения
  * Получим:
- * a * (a_x + 0.25) - e^(-4x) = k
- * Отсюда выразим x
- * e^(-4x) = a * (a_x + 0.25) - k
- * -4x = ln(a * (a_x + 0.25) - k)
- * x = -ln(a * (a_x + 0.25) - k)/4
+ * a * x = k
+ * e^(-4*a_x) - e^(-4x) + a * a_x = k
+ * Отсюда выразим x:
+ * x = k / a | [0; a_x]
+ * x = -ln(a * (a_x + 0.25) - k) / 4 | (a_x; +inf)
  *
- * x = a * x         | [0; a_x]
- * x = -ln(a * (a_x + 0.25) - k)/4 | [a_x; +inf)
- *
- * И еще магии:
- * Функция случайной величины k имеет такое же распределение, что и сама
- * случайная величина k (Насколько помню)
  */
+
+const double a = 2;
 
 double f(double x, double a_x) {
   double res = 0.0;
+  double r = 0.0;
+  double _a_x = -ln(a / 4) / 4;
   if (a_x < 0) {
-    if (x <= -ln(a / 4) / 4) {
+    // return 1 - pow(e, -4 * (x));
+    if (x < _a_x) {
       return a;
     } else {
       return 4 * pow(e, -4 * x);
-    }  // return 4 - 4 * pow(e, -x);
+    }
   }
-  if (x < 0) {
-    return 0;
-  } else if (x < a_x) {
-    return x / a;
+
+  a_x = -ln(a / 4) / 4;
+  if (x < a * a_x) {
+    res = x / a;
   } else {
-    // res = a * (a_x + 0.25) - pow(e, -4 * x);
-    res = -ln(a * (a_x + 0.25) - x) / 4;
-    // res = -ln(x) / 4;
-    // res = -ln((a * (a_x + 1) - x) / 4);
-    // res = -ln(x / 4);
-    return res;
+    r = a_x * a  + a * 0.25 - x;
+    if (r < 0) {
+      res = -1;
+      // res = -ln(-a * 0.25 - a_x * a + x) / 4;
+    } else {
+      res = -ln(r) / 4;
+    }
+    // res = x;
   }
+  return res;
 }
 
 int main(int argc, char** argv) {
   long long int i = 0;
   long long int j = 0;
-  long long int count = 0;
-  int min = MIN * EPS;
-  int max = MAX * EPS;
   double r = 0.0;
   double res = 0.0;
-  double a_x = 0.0;
-  double* array = (double*)calloc(CNT, sizeof(double));
-  srand(time(NULL));
+  // srand(time(NULL));
   // a_x = 4 * pow(e, -a);
-  a_x = -ln(a / 4) / 4;
   // a_x = -ln(a / 4);
   // fprintf(stdout, "+0\t%lf\n", a_x);
   for (i = 0; i < CNT; ++i) {
-    r = ((double)rand()) / RAND_MAX;
-    // r = r / EPS;
-    res = ((int)(f(r, a_x) * EPS)) / EPS;
-    // index = (int)(res * EPS);
-    // printf("%d\n", index);
-    // array[i] = res;
-    fprintf(stdout, "%ld\t%lf\n", i, res);
-    // ++array[index];
-    // fprintf(stderr, "%lld\n", i);
-    fprintf(stderr, "%lf\t%lf\n", r, f(r, -1));
+    r = ((double)rand()) / ((double)RAND_MAX);
+    res = f(r, 1);
+    fprintf(stdout, "%lf\n", res);
+    res = f(r, -1);
+    fprintf(stderr, "%lf\t%lf\n", r, res);
   }
-
-  free(array);
   return 0;
 }
